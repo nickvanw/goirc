e9a1183e328d4521625a6b20be7c914e79e69d7c
Nick Van Wiggeren <nick@facepwn.com>
2014-03-30 14:49:29 -0700

    Add unreachable code back (will work w/ later)

--- conn.go
+++ conn.go
@@ -30,6 +30,7 @@ func Connect(addr string, port int) (*Conn, error) {
 			return con, nil
 		}
 	}
+	return nil, errors.New("Exceeded Retry Count!")
 }
 
 func (c *Conn) ReadLine() (string, error) {
